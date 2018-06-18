3bc066ee59b4bb1b05402c8ca148036aee587fa9
m31271n <m31271n@users.noreply.github.com>
2018-06-18 13:12:57 +0800

    feat: check config existence

--- src/config.js
+++ src/config.js
@@ -4,21 +4,59 @@ const path = require('path');
 const os = require('os');
 
 const configFile = path.join(os.homedir(), '.qcup.json');
+const config = require(configFile);
 
-module.exports = require(configFile);
+checkConfigExistence(config);
+
+function checkConfigExistence(config) {
+  const fields = ['SecretId', 'SecretKey', 'Bucket', 'Region'];
+  const missingFields = [];
+
+  for (const field of fields) {
+    if (!config[field]) {
+      missingFields.push(field);
+    }
+  }
+
+  if (missingFields.length !== 0) {
+    printMissingFields(missingFields);
+    // eslint-disable-next-line
+    process.exit(1);
+  }
+
+  function printMissingFields(fields) {
+    const feedback = [
+      `check ${configFile}, ensure following fields is provided:`,
+    ];
+
+    fields.forEach(field => {
+      feedback.push(`* ${field}`);
+    });
+
+    // eslint-disable-next-line
+    console.error(feedback.join('\n'));
+  }
+}
+
+const { SecretId, SecretKey, Bucket, Region } = config;
+
+module.exports = {
+  auth: {
+    SecretId,
+    SecretKey,
+  },
+  location: {
+    Bucket,
+    Region,
+  },
+};
+
+// basic concepts: https://cloud.tencent.com/document/product/436/6225
+// availible regions: https://cloud.tencent.com/document/product/436/6224"
 
 // {
-//   "authInfo": {
-//     "comment":
-//       "basic concepts: https://cloud.tencent.com/document/product/436/6225",
-//     "SecretId": "xxx",
-//     "SecretKey": "xxx"
-//   },
-//   "location": {
-//     "comment":
-//       "availible regions: https://cloud.tencent.com/document/product/436/6224",
-//     "Bucket": "xxx",
-
-//     "Region": "xxx"
-//   }
+//   "SecretId": "AKIDBGA3gcuwPglgS3pfKpTzLYaQ4b4P5YO2",
+//   "SecretKey": "qdFg096tUyIUXO3Xqwsj2nS1SmwukOrC",
+//   "Bucket": "qcup-test-1252291541",
+//   "Region": "ap-guangzhou"
 // }
