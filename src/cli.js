e522433a25ad75b806b8ab024f99ef4f1c6ee9e3
m31271n <m31271n@users.noreply.github.com>
2018-07-06 18:46:43 +0800

    feat: seperate load / gen-config command

--- src/cli.js
+++ src/cli.js
@@ -4,49 +4,58 @@
 
 const path = require('path')
 const yargs = require('yargs')
+const qcup = require('.')
+const generateConfigSample = require('./generate-config')
 
 const argv = yargs
   .usage(
     [
       'Usage: $0',
+      '<command>',
       '-s [source directory]',
       '-t [target directory]',
       '-c [concurrency]',
     ].join(' ')
   )
-  .option('s', {
-    demandOption: true,
-    describe: 'source directoy contains files waiting for uploading',
-    type: 'string',
+  .command('load', 'upload files', function(yargs) {
+    return yargs
+      .option('s', {
+        describe: 'source directoy contains files waiting for uploading',
+        type: 'string',
+      })
+      .option('t', {
+        describe: 'target directory',
+        type: 'string',
+      })
+      .option('c', {
+        default: 5,
+        describe: 'concurrent tasks',
+        type: 'number',
+      })
+      .demandOption(
+        ['s', 't'],
+        'Please provide both source and target to work with this tool.'
+      )
+      .example('$0 load -c 8 -s /local/path/to/assets -t project-name')
   })
-  .option('t', {
-    demandOption: true,
-    describe: 'target directory',
-    type: 'string',
-  })
-  .option('c', {
-    demandOption: false,
-    default: 5,
-    describe: 'concurrent tasks',
-    type: 'number',
-  })
-  .option('g', {
-    demandOption: false,
-    describe: 'generate config sample',
-    type: 'boolean',
-  })
+  .command('gen-config', 'generate sample configuration')
   .help('h').argv
 
-if (argv.g) {
-  const generateConfigSample = require('./generate-config')
-  generateConfigSample()
-} else {
-  const qcup = require('.')
-  const sourceDirectory = path.isAbsolute(argv.s)
-    ? argv.s
-    : path.join(process.cwd(), argv.s)
-  const targetDirectory = argv.t
-  const concurrency = argv.c
+const command = argv._[0]
+switch (command) {
+  // eslint-disable-next-line
+  case 'load':
+    const sourceDirectory = path.isAbsolute(argv.s)
+      ? argv.s
+      : path.join(process.cwd(), argv.s)
+    const targetDirectory = argv.t
+    const concurrency = argv.c
 
-  qcup(targetDirectory, sourceDirectory, concurrency)
+    qcup(targetDirectory, sourceDirectory, concurrency)
+    break
+  case 'gen-config':
+    generateConfigSample()
+    break
+  default:
+    yargs.showHelp()
 }
